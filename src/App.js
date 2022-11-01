84454ca6a973365540e6bd4bca1377fc4dafc22c
Manpreet <mks221987@gmail.com>
2022-11-01 17:25:00 -0500

    Updated

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
     <>
     <BrowserRouter basename="my-portfolio">
     <Routes>
-      <Route  path="/" element={<Home/>} />
+      <Route  path="/my-portfolio" element={<Home/>} />
       <Route  path="/project" element={<Project/>} />
       <Route  path="/about" element={<About/>} />
       <Route  path="/contact" element={<Contact/>} />
