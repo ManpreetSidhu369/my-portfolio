ba01e950b05e7e318848ea83f12d709db86d6e4c
Manpreet <mks221987@gmail.com>
2022-11-01 17:17:31 -0500

    Commited

--- src/App.js
+++ src/App.js
@@ -4,18 +4,20 @@ import Home from "./routes/Home";
 import About from "./routes/About";
 import Project from "./routes/Project";
 import Contact from "./routes/Contact";
-import { Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 
 function App() {
   return (
     <>
+    <BrowserRouter basename="my-portfolio">
     <Routes>
       <Route  path="/" element={<Home/>} />
       <Route  path="/project" element={<Project/>} />
       <Route  path="/about" element={<About/>} />
       <Route  path="/contact" element={<Contact/>} />
     </Routes>
+    </BrowserRouter>
     </>
     
   );
